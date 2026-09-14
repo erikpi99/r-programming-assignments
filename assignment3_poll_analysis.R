01d348c091a98d3996e2c21930d051d81a23edae
erikpi99 <epihl@usf.edu>
2026-09-13 21:25:39 -0400

    Add Assignment 3 poll analysis

---
+++ assignment3_poll_analysis.R
@@ -0,0 +1,27 @@
+# Assignment # 3: Assignment #3: Analyzing 2016 data “Poll” Data in R
+
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC_poll   <- c(  4,      62,      51,    21,      2,        14,       15)
+CBS_poll   <- c( 12,      75,      43,    19,      1,        21,       19)
+
+df_polls <- data.frame(Name, ABC_poll, CBS_poll)
+
+str(df_polls)
+head(df_polls)
+
+mean(df_polls$ABC_poll)
+median(df_polls$CBS_poll)
+range(df_polls[, c("ABC_poll","CBS_poll")])
+
+df_polls$Diff <- df_polls$CBS_poll - df_polls$ABC_poll
+df_polls
+
+library(ggplot2)
+
+ggplot(df_polls, aes(x = ABC_poll, y = CBS_poll)) +
+  geom_point() +
+  labs(
+    title = "ABC vs CBS Poll Results",
+    x = "ABC Poll",
+    y = "CBS Poll"
+  )
